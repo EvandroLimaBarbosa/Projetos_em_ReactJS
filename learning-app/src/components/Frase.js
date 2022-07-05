2059ae70d853065ab1c73b0802d91669ead9d3a9
Evandro Lima Barbosa <evandrolimabarbosa@hotmail.com>
2022-07-05 07:20:13 -0300

    start new app

---
+++ learning-app/src/components/Frase.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Frase() {
+  return (
+    <div>Frase</div>
+  )
+}
---
+++ learning-app/src/components/HelloWorld.js
@@ -0,0 +1,11 @@
+function HelloWorld (){
+
+    return (
+        <div>
+            <h1>Meu primeiro Componente</h1>
+        </div>
+    )
+
+}
+
+export default HelloWorld
