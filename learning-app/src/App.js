bc4302010f72902d5d59aff219ae2cd6d3bca4f5
Evandro Lima <96010876+EvandroLimaBarbosa@users.noreply.github.com>
2022-07-27 23:16:09 -0300

    Update App.js

--- learning-app/src/App.js
+++ learning-app/src/App.js
@@ -12,6 +12,40 @@ function App() {
       <NavBar/>
         <Routes/>
     </Router>
+
+
+{/* <ListaManual/>
+      <RenderList/>
+
+
+        <Condicional/>
+        <Form />
+        <Evento numero={1} />
+      <div className="ContainerApp">
+        <Pessoa
+          nome="Evandro"
+          idade="22"
+          profissao="Programador"
+          foto="https://avatars.githubusercontent.com/u/96010876?s=400&u=1dff6ade3e9ddc2696e711a15c20a9ef784c4838&v=4"
+        />
+        <Pessoa
+          nome="Leonardo"
+          idade="17"
+          profissao="Programador"
+          foto="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_l-yO-axUTUnTOxJqTbbO-vJ6RZ5XC6X44A&usqp=CAU"
+        />
+        <Pessoa />
+        <Pessoa />
+        <Pessoa />
+        <Pessoa />
+        <Pessoa />
+        <Pessoa />
+      </div>
+        <List />*/}
+
+
+
+
     </div>
   );
 }
