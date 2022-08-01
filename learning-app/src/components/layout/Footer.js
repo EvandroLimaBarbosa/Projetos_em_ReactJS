7da5399763964ff4253908c960d3d36b8a35bbe6
Evandro Lima <96010876+EvandroLimaBarbosa@users.noreply.github.com>
2022-08-01 20:37:22 -0300

    Update Footer.js

--- learning-app/src/components/layout/Footer.js
+++ learning-app/src/components/layout/Footer.js
@@ -1,19 +1,24 @@
-import React from 'react'
+import React from "react";
 import styled from "styled-components";
 
 export default function Footer() {
   const Div = styled.div`
-  display:flex;
-  align-content: flex-end;
-  justify-content: end;
-  margin: 0;
-  padding: 1em;
-  background: black;
-  min-height: 1em;
-`;
+    margin-top: auto;
+    padding: 1em;
+    background: black;
+    min-height: 1em;
+    width: 100%;
+    position: fixed;
+    bottom: 0;
+    p {
+      margin: auto;
+      color: white;
+      background: none;
+    }
+  `;
   return (
     <Div>
       <p>Evandro</p>
     </Div>
-  )
+  );
 }
