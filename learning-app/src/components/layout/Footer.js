659bd5dc530b68982155dab70494d713438f67c9
Evandro Lima <96010876+EvandroLimaBarbosa@users.noreply.github.com>
2022-07-31 22:57:57 -0300

    navbar style

--- learning-app/src/components/layout/Footer.js
+++ learning-app/src/components/layout/Footer.js
@@ -1,7 +1,19 @@
 import React from 'react'
+import styled from "styled-components";
 
 export default function Footer() {
+  const Div = styled.div`
+  display:flex;
+  align-content: flex-end;
+  justify-content: end;
+  margin: 0;
+  padding: 1em;
+  background: black;
+  min-height: 1em;
+`;
   return (
-    <div>Footer</div>
+    <Div>
+      <p>Evandro</p>
+    </Div>
   )
 }
--- learning-app/src/components/layout/NavBar.js
+++ learning-app/src/components/layout/NavBar.js
@@ -1,19 +1,40 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 export default function NavBar() {
+  const Div = styled.div`
+    justify-content: right;
+    margin: 0;
+    padding: 1em;
+    background: #001B3B;
+    min-height: 1em;
+    box-shadow: 2px 2px 2px 1px rgba(0, 0, 0, 0.2);
+    ul {
+      display: flex;
+      flex-direction: row;
+      justify-content:right;
+    }
+    li{
+      margin: 0;
+      padding: 0;
+      list-style:none;
+    }
+    a {
+      text-decoration: none;
+      color: black;
+      margin: .1em;
+      padding: .5em;
+      border-radius: .3em;
+      background: gray;
+     &:hover {
+      color: white;
+      background: gray;
+     }
+    }
+  `;
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "right",
-        margin: "0",
-        padding: "1em",
-        backgroundColor: "white",
-        minHeight: "1em",
-      }}
-    >
+    <Div>
       <ul>
         <li>
           <Link to="/">Home</Link>
@@ -34,6 +55,6 @@ export default function NavBar() {
           <Link to="/List">List</Link>
         </li>
       </ul>
-    </div>
+    </Div>
   );
 }
